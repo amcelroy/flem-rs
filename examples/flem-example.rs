d76afdc7f838c20a9f7ccb97b732d32be78cafc0
Austin McElroy <mcelroy.austin@gmail.com>
2022-05-11 08:53:29 -0500

    Cleaned up compilers warnings;improved example

--- examples/flem-example.rs
+++ examples/flem-example.rs
@@ -1,18 +1,47 @@
 use flem;
-use flem::FlemPacket;
-use flem::FlemInterface;
-use flem::FlemDataId;
+use flem::*;
 
-fn flem_error_handler(i: &FlemInterface, p: &mut FlemPacket, status: u16) {
+static mut TX_PACKET: Option<FlemPacket> = None;
+
+fn flem_error_handler(_i: &FlemInterface, _p: &mut FlemPacket, status: u16) {
     println!("Error detected: {}", status);
 }
 
-fn flem_valid_handler(i: &FlemInterface, p: &mut FlemPacket) {
+fn flem_valid_handler(_i: &FlemInterface, p: &mut FlemPacket) {
     println!("Packet success, work your magic!");
+
+    let result = match p.get_command() {
+        FlemCommands::FLEM_COMMAND_GET_SMART_DRAIN => {
+            //Note: In C, I have heap allocated rx and tx packets that are 
+            //global to the flem.c project and only accessed within the C file
+            //and not from other sources.
+            
+            unsafe {
+                let mut pp = TX_PACKET.unwrap();
+                pp.reset();
+                pp.set_command(p.get_command());
+                pp.set_status(FlemStatus::FLEM_INFO_PROCESSED);
+                pp.checksum(true);
+                TX_PACKET = Some(pp);
+            }
+
+            //let mut tx_packet = FlemPacket::new();
+            //tx_packet.reset();
+            //tx_packet.set_command(p.get_command());
+            //tx_packet.set_status(FlemStatus::FLEM_INFO_PROCESSED);
+            //tx_packet.checksum(true);
+            Ok(())
+        },
+        _ => { Err("Command not recognized") }
+    };
+
+    result.unwrap();
 }
 
 fn main() {
-    let special_functions = [0u32; 10];
+    unsafe {
+        TX_PACKET = Some(FlemPacket::new());
+    }
 
     let flem_interface = flem::FlemInterface {
         id: FlemDataId::new("FLEM Rust Demo", "0.0.1", &[0u32; 10]).unwrap(),
@@ -20,27 +49,22 @@ fn main() {
         valid_handler: flem_valid_handler,
     };
 
-    let x = flem::FlemCommands::FLEM_SET_OFFSET;    
-
     let mut rx = flem::FlemPacket::new();
     let mut tx = flem::FlemPacket::new();
 
-    println!("Packet data length: {}", rx.getDataArray().len());
+    println!("Packet data length: {}", rx.get_data_array().len());
     
     println!("Packet length: {}", rx.length());
 
-    tx.setStatus(0);
-    tx.setCommand(1);
+    tx.set_status(0);
+    tx.set_command(FlemCommands::FLEM_COMMAND_GET_SMART_DRAIN);
 
     let crc = tx.checksum(true);
     println!("Packet CRC: {}", crc);
-
-    println!("Packet as u8: {:?}", tx.asU8Array());
     
-    for i in 0..tx.length() {
+    for _i in 0..tx.length() {
         let next_byte = tx.get_next_byte(&flem_interface).unwrap();
-        println!("TX Packet byte: {}", next_byte);
-        rx.add_byte(&flem_interface, &next_byte);
+        rx.add_byte(&flem_interface, &next_byte).unwrap();
     }
 
     println!("Example with max data payload");
@@ -49,21 +73,21 @@ fn main() {
     tx.reset();
     rx.reset();
 
-    tx.setCommand(1);
+    tx.set_command(FlemCommands::FLEM_COMMAND_GET_SMART_DRAIN);
     let mut some_data: Vec<u8> = Vec::with_capacity(FlemPacket::FLEM_MAX_DATA_SIZE as usize);
    
     for a in 0..FlemPacket::FLEM_MAX_DATA_SIZE {
         some_data.push(a as u8);
     }
 
-    tx.addData(&some_data);
+    tx.add_data(&some_data).unwrap();
     tx.checksum(true);
 
     println!("Packet length: {}", tx.length());
 
-    for i in 0..tx.length() {
+    for _i in 0..tx.length() {
         let next_byte = tx.get_next_byte(&flem_interface).unwrap();
-        rx.add_byte(&flem_interface, &next_byte);
+        rx.add_byte(&flem_interface, &next_byte).unwrap();
     }
 
 }
